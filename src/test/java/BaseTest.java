2b6063fcf1d25c6d65132246a17f9cbd85f4de7e
Sergey <pimenovoffical@gmail.com>
2025-08-05 19:40:13 +0300

    added BaseTest,update pom.xml and editing README.md

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class BaseTest {
+    protected static final String BASE_URL = "http://82.142.167.37:4881/";
+
+    @BeforeMethod
+    public void setup() {
+        Configuration.browser = "chrome";
+        Configuration.timeout = 5000;
+        Configuration.headless = false;
+        Configuration.clickViaJs = true;
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--maximized");
+        options.addArguments("--incognito");
+        Configuration.browserCapabilities = options;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if(getWebDriver() !=null) {
+            closeWebDriver();
+        }
+    }
+}
