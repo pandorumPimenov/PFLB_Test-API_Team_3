f81b72193f6c360f00e6f8010186623f17392309
Natalia <deriusheva785@gmail.com>
2025-08-08 14:25:37 +0300

    Исправила локаторы на селенид в page UsersReadAll

--- src/test/java/pages/UsersReadAllPage.java
+++ src/test/java/pages/UsersReadAllPage.java
@@ -1,12 +1,13 @@
 package pages;
 
-import io.qameta.allure.Step;
-import org.openqa.selenium.By;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
 
 /*
 Создать пользователя и проверить отображение данных в таблице:
@@ -18,24 +19,24 @@ sex
 money   - найти его там нереально может пропустить?
 */
 public class UsersReadAllPage extends BasePage {
-    private final By USER_TITLE_UP_MENU = By.id("basic-nav-dropdown");
-    private final By DROPDOWN_LINK_READ_ALL = By.xpath("//div[@class='dropdown-menu'] /descendant:: a[contains (text()='Read all')]");
-    private final By USERS_READ_ALL_RELOAD_BUTTON = By.xpath("//button[contains(text(),'Reload')]");
-    private final By USERS_READ_ALL_ID_BUTTON = By.xpath("//button[contains(text(),'ID')]");
-    private final By USERS_READ_ALL_FIRST_NAME_BUTTON = By.xpath("//button[contains(text(),'First')]");
-    private final By USERS_READ_ALL_LAST_NAME_BUTTON = By.xpath("//button[contains(text(),'Last')]");
-    private final By USERS_READ_ALL_AGE_BUTTON = By.xpath("//button[contains(text(),'Age')]");
-    private final By USERS_READ_ALL_SEX_BUTTON = By.xpath("//button[contains(text(),'Sex')]");
-    private final By USERS_READ_ALL_MONEY_BUTTON = By.xpath("//button[contains(text(),'Money')]");
-    private final By USERS_READ_ALL_TABLE_ID_TITLE = By.xpath("//th[contains(text(),'ID')]");
-    private final By USERS_READ_ALL_FIRST_NAME_TITLE = By.xpath("//th[contains(text(),'First')]");
-    private final By USERS_READ_ALL_LAST_NAME_TITLE = By.xpath("//th[contains(text(),'Last')]");
-    private final By USERS_READ_ALL_AGE_TITLE = By.xpath("//th[contains(text(),'Age')]");
-    private final By USERS_READ_ALL_SEX_TITLE = By.xpath("//th[contains(text(),'Sex')]");
-    private final By USERS_READ_ALL_MONEY_TITLE = By.xpath("//th[contains(text(),'Money')]");
-    private final By USERS_READ_ALL_TABLE_BODY = By.tagName("tbody");
-    private final By USERS_READ_ALL_TABLE_CELL_ID = By.xpath("//td[contains(text(), '3')]");
-    private final By USERS_READ_ALL_TABLE_CELL_FIRST_NAME = By.cssSelector("tbody tr td:nth-child(2)");// втроая с именем
+    private final SelenideElement USER_TITLE_UP_MENU = $("#basic-nav-dropdown");
+    private final SelenideElement DROPDOWN_LINK_READ_ALL = $x("//div[@class='dropdown-menu'] /descendant:: a[contains (text()='Read all')]");
+    private final SelenideElement USERS_READ_ALL_RELOAD_BUTTON = $x("//button[contains(text(),'Reload')]");
+    private final SelenideElement USERS_READ_ALL_ID_BUTTON = $x("//button[contains(text(),'ID')]");
+    private final SelenideElement USERS_READ_ALL_FIRST_NAME_BUTTON = $x("//button[contains(text(),'First')]");
+    private final SelenideElement USERS_READ_ALL_LAST_NAME_BUTTON = $x("//button[contains(text(),'Last')]");
+    private final SelenideElement USERS_READ_ALL_AGE_BUTTON = $x("//button[contains(text(),'Age')]");
+    private final SelenideElement USERS_READ_ALL_SEX_BUTTON = $x("//button[contains(text(),'Sex')]");
+    private final SelenideElement USERS_READ_ALL_MONEY_BUTTON = $x("//button[contains(text(),'Money')]");
+    private final SelenideElement USERS_READ_ALL_TABLE_ID_TITLE = $x("//th[contains(text(),'ID')]");
+    private final SelenideElement USERS_READ_ALL_FIRST_NAME_TITLE = $x("//th[contains(text(),'First')]");
+    private final SelenideElement USERS_READ_ALL_LAST_NAME_TITLE = $x("//th[contains(text(),'Last')]");
+    private final SelenideElement USERS_READ_ALL_AGE_TITLE = $x("//th[contains(text(),'Age')]");
+    private final SelenideElement USERS_READ_ALL_SEX_TITLE = $x("//th[contains(text(),'Sex')]");
+    private final SelenideElement USERS_READ_ALL_MONEY_TITLE = $x("//th[contains(text(),'Money')]");
+    private final SelenideElement USERS_READ_ALL_TABLE_BODY = $("tbody");
+    private final SelenideElement USERS_READ_ALL_TABLE_CELL_ID = $x("//td[contains(text(), '3')]");
+    private final SelenideElement USERS_READ_ALL_TABLE_CELL_FIRST_NAME = $("tbody tr td:nth-child(2)");// втроая с именем
 
     @Step("открытие страницы Users_Read_all")
     public UsersReadAllPage openUsersReadAllPage(){
