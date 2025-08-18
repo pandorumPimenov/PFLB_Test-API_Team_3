3702f31942b88d97dd2196e482d5d4b2b415f6df
Sergey <pimenovoffical@gmail.com>
2025-08-18 18:38:00 +0300

    update allure annotation api\CartTest

--- src/test/java/tests/api/CarTest.java
+++ src/test/java/tests/api/CarTest.java
@@ -2,6 +2,8 @@ package tests.api;
 
 import adapters.CarAdapter;
 import dto.api.Car;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,6 +12,8 @@ public class CarTest {
     long id;
 
     @Test(priority = 1)
+    @Description("Test verifies successful creation of a new car via API")
+    @Owner("Андреев Дмитрий")
     public void createCar() {
         CarAdapter carAdapter = new CarAdapter();
         Car car = Car.builder()
@@ -24,6 +28,8 @@ public class CarTest {
     }
 
     @Test(priority = 2)
+    @Owner("Андреев Дмитрий")
+    @Description("Test verifies ability to retrieve car information by ID")
     public void getCar() {
         CarAdapter carAdapter = new CarAdapter();
         Car get = carAdapter.getCar(id);
@@ -32,6 +38,8 @@ public class CarTest {
 
 
     @Test(priority = 3)
+    @Owner("Андреев Дмитрий")
+    @Description("Test verifies successful update of existing car details")
     public void updateUser() {
         CarAdapter carAdapter = new CarAdapter();
         Car car = Car.builder()
@@ -45,6 +53,8 @@ public class CarTest {
     }
 
     @Test(priority = 4)
+    @Description("Test verifies successful deletion of a car")
+    @Owner("Андреев Дмитрий")
     public void deleteCar() {
         CarAdapter carAdapter = new CarAdapter();
         carAdapter.deleteCar(id);
